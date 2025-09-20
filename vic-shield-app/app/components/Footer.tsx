9b2b9e37d7daf1468145058634c935a2c601bbdd
Michael <83624415+michaelbera@users.noreply.github.com>
2025-09-20 21:29:15 +0700

    _

--- vic-shield-app/app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer: React.FC = () => {
-  return (
-    <footer className="py-10 px-6 bg-[#0d0f1a] text-center text-gray-400">
-      <p>© {new Date().getFullYear()} VicShield. All rights reserved.</p>
-    </footer>
-  )
-}
-
-export default Footer
