6d320b0addfa27686b3ebac0adb43b0a55bc8679
Michael <83624415+michaelbera@users.noreply.github.com>
2025-09-20 21:39:42 +0700

    init route

--- vicshield-app/src/App.tsx
+++ vicshield-app/src/App.tsx
@@ -1,8 +1,33 @@
-import "./App.css";
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
 import HomePage from "./home/Page";
 
+import "./App.css";
+import Contracts from "./pages/contracts/Page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Outlet />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/contracts", element: <Contracts /> },
+      { path: "*", element: <Navigate to="/" /> },
+    ],
+  },
+]);
+
 function App() {
-  return <HomePage />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ vicshield-app/src/pages/contracts/Page.tsx
@@ -0,0 +1,5 @@
+const Contracts = () => {
+  return <div>Page</div>;
+};
+
+export default Contracts;
